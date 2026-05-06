8f1ed8ff88446b977945730bbc667b9daf86a563
kk241228 <didada9898@163.com>
2026-05-06 19:01:54 +0800

    更新游戏资源

---
+++ settings.js
@@ -0,0 +1,16 @@
+window.boot = {
+    debug: false,
+    settings: {
+        debugMode: false,
+        launchScene: "db://assets/framework/scene/App.fire",
+        id: "shared",
+        jsList: [
+            "src/project.js",
+            "main.js"
+        ],
+        moduleList: ["cocos2d"],
+        platform: "web-mobile",
+        frameRate: 60,
+        showFPS: false
+    }
+};
